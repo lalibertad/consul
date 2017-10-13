7930e004501fc9163e5a1f903d6f6fbbbe7b461d
Linder Reyna <linder_reynae@hotmail.com>
2017-10-13 08:41:31 -0500

    do not allow name change

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -24,9 +24,9 @@ class AccountController < ApplicationController
     def account_params
       if @account.organization?
         params.require(:account).permit(:phone_number, :email_on_comment, :email_on_comment_reply, :newsletter,
-                                        organization_attributes: [:name, :responsible_name])
+                                        organization_attributes: [:name])
       else
-        params.require(:account).permit(:username, :public_activity, :public_interests, :email_on_comment, :email_on_comment_reply,
+        params.require(:account).permit(:public_activity, :public_interests, :email_on_comment, :email_on_comment_reply,
                                         :email_on_direct_message, :email_digest, :newsletter, :official_position_badge)
       end
     end
