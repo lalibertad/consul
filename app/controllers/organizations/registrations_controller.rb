7d63f690c20f94b7bb81aef506d646f250728b82
Linder Reyna <linder_reynae@hotmail.com>
2017-10-17 12:53:45 -0500

    registry update of organizations

--- app/controllers/organizations/registrations_controller.rb
+++ app/controllers/organizations/registrations_controller.rb
@@ -32,7 +32,28 @@ class Organizations::RegistrationsController < Devise::RegistrationsController
   private
 
     def sign_up_params
-      params.require(:user).permit(:email, :password, :phone_number, :password_confirmation, :terms_of_service,
+      begin
+        params[:user][:document_type] = "1"
+        response = HTTParty.get("#{Rails.application.secrets.api_reniec}?numDni=#{params[:user][:document_number]}")
+        if response.body != "null"
+          datos = JSON.parse(response.body)
+          params[:user][:date_of_birth] = DateTime.strptime(datos["FENAC"] + "120000", "%Y%m%d%H%M%S")
+          if datos["SEXO"] == "M"
+            params[:user][:gender] = "Male"
+          else
+            params[:user][:gender] = "Female"
+          end
+          nombres = datos["NOMBRES"]
+          nombres.strip!
+          appat = datos["APPAT"]
+          appat.strip!
+          apmat = datos["APMAT"]
+          apmat.strip!
+          params[:user][:username] = "#{nombres}" + " " + "#{appat}" + " " + "#{apmat}"
+          params[:user][:organization_attributes][:responsible_name] = "#{nombres}" + " " + "#{appat}" + " " + "#{apmat}"
+        end
+      end
+      params.require(:user).permit(:document_number, :document_type, :username, :email, :password, :phone_number, :password_confirmation, :terms_of_service, :date_of_birth, :gender,
                                    organization_attributes: [:name, :responsible_name])
     end
 
