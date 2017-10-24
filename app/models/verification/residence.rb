e9d1c568d5431b799b8fbb6001b829a8f244c33f
Linder Reyna <linder_reynae@hotmail.com>
2017-10-24 14:07:17 -0500

    update residence

--- app/models/verification/residence.rb
+++ app/models/verification/residence.rb
@@ -48,6 +48,7 @@ class Verification::Residence
                 confirmed_phone: Faker::PhoneNumber.phone_number,
                 residence_verified_at: Time.current)
     rescue
+      errors.add(:terms_of_service, "error desconocido")
       return false
     end
   end
