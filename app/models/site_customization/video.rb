b032add7c47e0797fd5737acd83d2da96928dc7b
Linder Reyna <linder_reynae@hotmail.com>
2018-01-05 14:44:42 -0500

    update video

--- app/models/site_customization/video.rb
+++ app/models/site_customization/video.rb
@@ -2,7 +2,7 @@ class SiteCustomization::Video < ActiveRecord::Base
   has_attached_file :video
 
   validates :name, presence: true, uniqueness: true
-  validates_attachment_content_type :video, content_type: ['application/x-shockwave-flash', 'application/x-shockwave-flash', 'application/flv', 'video/x-flv']
+  validates_attachment_content_type :video, content_type: ["application/mp4", "audio/mp4", "video/mp4", "video/vnd.objectvideo"]
 
   def self.one_video
     find_by(name: "welcome_video") || create!(name: "welcome_video")
