2b21fd3dafb540ab6a9ddb2197aee474a5d96ad6
Linder Reyna <linder_reynae@hotmail.com>
2018-01-18 11:10:59 -0500

    change to custom

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,3 +4,38 @@
 // * Spanish: https://github.com/consul/consul/blob/master/CUSTOMIZE_ES.md#javascript
 //
 //
+function showUser(id) {
+  $.ajax({
+    url: '/more-show/' + id,
+    success: function (response) {
+      if (response.estado === 1){
+        $('#uname').text(response.citizen.username);
+        $('#udoc').text(response.citizen.document_number);
+        $('#uema').text(response.citizen.email);
+        if (response.zone){
+          $('#udist').text(response.zone.name);
+          $('#uham').text(response.citizen.hamlet_or_urbanization);
+        } else {
+          $('#udist').text('');
+          $('#uham').text('');
+        }
+        $('#uyea').text((new Date().getFullYear() - new Date(response.citizen.date_of_birth).getFullYear()));
+        $('#usex').text(response.citizen.gender);
+        $('#upro').text(response.citizen.profession);
+        $('#showUsers').foundation('open');
+      } else {
+        var alertBox = '<div id="alert" data-alert="" class="notice-container callout-slide" data-closable="">' +
+          '    <div class="callout notice alert">' +
+          '      <button class="close-button" aria-label="Cerrar" type="button" data-close="">' +
+          '        <span aria-hidden="true">×</span>' +
+          '      </button>' +
+          '      <div class="notice-text">' +
+          '        Información del usuario no disponible.' +
+          '      </div>' +
+          '    </div>' +
+          '  </div>';
+        $(".admin-content").append(alertBox).foundation();
+      }
+    }
+  });
+}
