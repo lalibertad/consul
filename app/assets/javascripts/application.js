581f6ae6cf8dc6afa54613fc04e2303509afa291
Linder Reyna <linder_reynae@hotmail.com>
2018-01-18 11:04:08 -0500

    update view users

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -153,8 +153,11 @@ function showUser(id) {
                 $('#udoc').text(response.citizen.document_number);
                 $('#uema').text(response.citizen.email);
                 if (response.zone){
-                    $('#udist').text(response.zone.name);
-                    $('#uham').text(response.citizen.hamlet_or_urbanization);
+                  $('#udist').text(response.zone.name);
+                  $('#uham').text(response.citizen.hamlet_or_urbanization);
+                } else {
+                  $('#udist').text('');
+                  $('#uham').text('');
                 }
                 $('#uyea').text((new Date().getFullYear() - new Date(response.citizen.date_of_birth).getFullYear()));
                 $('#usex').text(response.citizen.gender);
